2cda6f74395b7e7fce02abfc226e9df1a92ab1c5
jac0320 <sitew@clemson.edu>
2016-02-11 11:53:53 -0500

    Made progress on generating BLOCK DISCRETE sto file

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import utility
 # Open Excel Sheet
 class interface(object):
     def __init__(self):
+        self.inputMode = None;
         self.iniFlag = 0;
         self.endFlag = 0;
         self.testFlag = 0;
@@ -26,107 +27,27 @@ class interface(object):
         self.stoType = None;
         self.pfLength = 7;
 
-    def incomplete(self):
+    def _incomplete(self):
         raise NotImplementedError("Please implement this method!");
 
+    def TEST(self):
+        self.stoType = 2;
+        self.inputMode = 'Excel';
+        self.rcPath = "/Users/jac0320/GitHub/stoGenerator/";
+        self.mpsPath = "/Users/jac0320/GitHub/stoGenerator/";
+        self.outputPath = "/Users/jac0320/GitHub/stoGenerator/";
+        self.refPath = "/Users/jac0320/GitHub/stoGenerator/ExampleWorkbook.xlsx";
+        self.pName = "stoGTester";
+
     def initialize(self):
-        # This is going to be the beginning of everything...
-        # User Input
-        # User Input Part:
-        #print "Please Select Stoch File Type(Choose #):";
-        #print "1.INDEP";
-        #print "2.BLOCKS";
-        #print "Generate sto file with (type X to quit)"
+        print "Welcome!";
 
-        # stoType = None;
-        # while stoType != "X":
-        #     stoType = raw_input();
-        #     clear_output();
-        #
-
-        # print "Insert refPath:"
-        # refPath = raw_input();
-        # if refPath == "X":
-        #     sys.exit();
-        # else:
-        #     clear_output();
-
-        # print "Please insert the stoch file path, (type X to quit)"
-        # stoPath = None;
-        # while stoPath != "X":
-        #     stoPath = raw_input("Insert Path");
-        #     clear_output();
-        #
-        # stoName = None;
-        # while stoName is None:
-        #     stoName = raw_input("Insert your problem's name");
-        #     clear_output();
-        #
-        # # Generate Target File
-        # if refPath == ".":
-        #     # Absolute Path in current folder
-        #     f = open(str(stoName), 'w');
-        # else:
-        #     # User Specified Path
-        #     fullStoPath = os.path.join(str(stoPath), stoName + ".sto");
-        #     f = open(fullStoPath, 'w');
-        # Call Class Function for Use
-        pass
-
-    def _indep_rhs_only(self):
-        mainObject = StoGenerator();
-        mainObject.discrete_generator();
-        pass
+        user_input = raw_input();
+        while user_input is not "X" and user_input is not None:
+             self.stoType = raw_input();
+             clear_output();
 
 
-class RCDictionary(interface):
-    def __init__(self):
-        self.rows = {};
-        self.cols = {};
-
-    def _collect_dictionary(self, pfLength, rcPath, pName):
-        # Open Rol File
-        rowF = open(rcPath+pName+'.row', 'r');
-        counter = 1;
-        for line in rowF:
-            line = line.rstrip('\r\n');
-            #ripLine = line.split('[');
-            #line = ripLine[0];
-            strFormat = '{0:0'+str(pfLength)+'}';
-            numberStr = strFormat.format(counter);
-            self.cols["R"+str(numberStr)] = line;
-            counter += 1;
-        # Open Col File
-        colF = open(rcPath+pName+'.col', 'r');
-        counter = 1;
-        for line in colF:
-            line = line.rstrip('\r\n');
-            #ripLine = line.split('[');
-            #line = ripLine[0];
-            strFormat = '{0:0'+str(pfLength)+'}';
-            numberStr = strFormat.format(counter);
-            self.rows["C"+str(numberStr)] = line;
-            counter += 1;
-        # Close row/col Files
-        rowF.close()
-        colF.close()
-        pass
-
-    def mps_translator(self, rcPath, pfLength, mpsPath, pName):
-        counter = 1;
-        self._collect_dictionary(pfLength,rcPath,pName);
-        mpsF = open(str(mpsPath)+str(pName)+'.mps','r');
-        newMpsF = open(str(mpsPath)+str(pName)+'_Tranlsated.mps','w');
-        for line in mpsF:
-            for key in self.rows:
-                line = line.replace(key, self.rows[key]);
-            for key in self.cols:
-                line = line.replace(key, self.cols[key]);
-            newMpsF.write(line);
-            print "Line ", counter, " Fixed..."
-            counter += 1;
-        mpsF.close()
-        newMpsF.close()
         pass
 
 
@@ -138,9 +59,13 @@ class StoGenerator(interface):
         self.rhsTotal = 0;
         self.rhs = [];
         self.rhsDist = [];
-
-    def _distribution_simulator(self):
-        pass
+        # For BLOCK-DISCRETE
+        self.blockTotal = 0;
+        self.rowsTotal = 0; #Assuming each block having same amount of rows
+        self.blockName = [];
+        self.blockRowName=[]
+        self.blockRowValue={}; #blockName are keys
+        self.blockProbs=[];
 
     def _collect_excel_distribution(self,dist_sheet):
         total_dist = dist_sheet.cell(0, 2).value;
@@ -154,7 +79,7 @@ class StoGenerator(interface):
 
     def _collect_excel_user_input(self,indicator_sheet):
         # Collect User's Input from Excel file and fill them into the f
-        if self.stoType == 1:
+        if self.stoType == 1: #DISCRETE FILE
             # Collect all random row amount
             self.rhsTotal = indicator_sheet.cell(1,4);
             # Collect all user indicated rows
@@ -162,7 +87,28 @@ class StoGenerator(interface):
                 self.rhs.append(indicator_sheet.cell(i+1,0));
             # Collect all user indicated distributions
             for i in range(self.rhsTotal):
-                self.rhsDist.append(indicator_sheet.cell(i+1,1));
+                self.rhsDist.append(indicator_sheet.cell(i+1,1))
+        if self.stoType == 2: #BLOCK FILE
+            #Collect total BLOCKs and total Rows
+            self.blockTotal = int(indicator_sheet.cell(0,0).value);
+            self.rowsTotal = int(indicator_sheet.cell(0,1).value);
+            #Collect BLOCK Names
+            self.blockName.append(self.blockTotal);    #keep the count in head
+            for i in range(1,self.blockTotal+1):
+                self.blockName.append(indicator_sheet.cell(i,0).value);
+            #Collect ROW Names
+            self.blockRowName.append(self.rowsTotal);
+            for i in range(1,self.rowsTotal+1):
+                self.blockRowName.append(indicator_sheet.cell(0,i+1).value);
+            #Collect PROBS Values
+            for i in range(1,self.blockTotal+1):
+                self.blockProbs.append(indicator_sheet.cell(i,1).value);
+            #Collect BLOCK-ROW Values
+            for i in range(1,self.blockTotal+1):
+                temp = [];
+                for j in range(1,self.rowsTotal+1):
+                    temp.append(indicator_sheet.cell(i,j+1).value);
+                self.blockRowValue[self.blockName[i]] = temp;
         pass
 
     def _write_discrete_sto_file(self):
@@ -171,24 +117,43 @@ class StoGenerator(interface):
         # Contruct File in Destination Location
         stoF = open(self.outputPath+fileName, 'w');
         # Write The Sto File Head
-        stoF.write('STOCH          '+fileName+'\n');
+        stoF.write('STOCH          '+self.pName+'\n');
         stoF.write('INDEP          DISCRETE\n');
         for i in range(self.rhsTotal):
             for j in range(self.rhsDist[i]):
                 rhsValue = self.rhsDist[i][0];
                 rhsProb = self.rhsDist[i][1];
-                self._write_space(stoF,5);
+                utility.write_space(stoF,5);
                 stoF.write('RHS');
-                self._write_space(stoF,7);
+                utility._write_space(stoF,7);
                 stoF.write(self.rhs[i]);
-                self._write_space(stoF,7);
+                utility._write_space(stoF,7);
                 stoF.write(rhsValue);
-                self._write_space(stoF,19);
+                utility._write_space(stoF,19);
                 stoF.write(rhsProb);
                 stoF.write('\n');
+        stoF.close()
 
+    def _write_block_sto_file(self):
+        #Construct File Name
+        fileName = self.pName + ".sto";
+        stoF = open(self.outputPath+fileName,'w');
+        # Write the Sto File Head
+        stoF.write('STOCH           '+self.pName+'\n');
+        stoF.write('BLOCKS          DISCRETE\n');
+        # Write Block Sections
+        for i in range(1,self.blockTotal+1):
+            stoF.write(' BL '+str(self.blockName[i])+'    PERIOD03        '+str(self.blockProbs[i-1])+'\n');
+            for j in range(1,self.rowsTotal):
+                utility.write_space(stoF,5);
+                stoF.write('RHS');
+                utility.write_space(stoF,9);
+                stoF.write(str(self.blockRowName[j]));
+                utility.write_space(stoF,11);
+                stoF.write(str(self.blockRowValue[self.blockName[i]][j]));
+                stoF.write('\n');
+        stoF.write('ENDATA');
 
-        # Write The Sto File Body
 
     def discrete_generator(self):
         if self.inputMode is 'Excel':
@@ -200,19 +165,26 @@ class StoGenerator(interface):
             # Cellect Distribution Knowledge from Excel File
             self._collect_excel_distribution(dist_sheet);
             if self.stoType == 1:
-                indicator_sheet = bloc_sheet;
+                indicator_sheet = disc_sheet;
             elif self.stoType == 2:
-                indicator_sheet = disc_sheet
+                indicator_sheet = bloc_sheet;
+
             # collect User's Indication from Excel file
-            self._collect_excel_user_input(self,indicator_sheet);
+            self._collect_excel_user_input(indicator_sheet);
+
+            # Write Informaiton to File with Fixed Format
+            if self.stoType == 1:
+                self._write_discrete_sto_file();
+            elif self.stoType == 2:
+                self._write_block_sto_file();
+
         pass
 
 # For Testing:
-test = interface();
-test.pName = "stoGTester";
-test.refPath = "/Users/jac0320/GitHub/stoGenerator/ExampleWorkbook.xlsx";
-test.rcPath = "/Users/jac0320/GitHub/stoGenerator/";
-test.mpsPath = "/Users/jac0320/GitHub/stoGenerator/";
-test.stoType = 1;
+stoG = StoGenerator();
+stoG.TEST();
+stoG.discrete_generator();
+
+
 
 
--- utility.py
+++ utility.py
@@ -1,6 +1,8 @@
 # Utilities of stoGenerator
 
-def write_space(self,fileObject,length):
+def write_space(fileObject,length):
     # Write Consecutive spaces in file
     for i in range(length):
         fileObject.write(' ');
+    pass
+
